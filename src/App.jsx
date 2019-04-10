b0577ff90efee60f8cabe50fcae83710f5ec1c84
Alejandro García Salas <garciasalasalejandro@gmail.com>
2019-04-09 18:30:12 -0700

    fix autoPlays typo

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ class App extends React.Component {
       <div className="container">
         <h1 className="header">Today</h1>
         {/* TODO 4. */}
-        <Timer mode="WORK" autoplays onSessionComplete={()=>console.log('complete!')} />
+        <Timer mode="WORK" autoPlays onSessionComplete={()=>console.log('complete!')} />
         {/*<SessionsCompletedCounter sessionsCompleted={} /> */}
       </div>
     );
